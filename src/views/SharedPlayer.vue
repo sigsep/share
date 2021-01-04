c7023266f9e726efd706ad984b29778d44a90363
Fabian-Robert Stöter <mail@faroit.com>
2021-01-04 15:49:57 +0100

    updated share player

--- src/views/SharedPlayer.vue
+++ src/views/SharedPlayer.vue
@@ -1,19 +1,18 @@
 <template>
-    <v-app id='app' :dark="dark" >
-
-        <v-app-bar
-                app
-                clipped-left
-        >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-            <v-toolbar-title>share.unmix.app</v-toolbar-title>
-        </v-app-bar>
-
-        <v-container fill-height fluid>
+    <v-app class="ma-0 pa-0" style="background-color: white;">
+        <v-container class="ma-0 pa-0" fill-height fluid>
             <v-row align="center"
                    justify="center">
                 <v-col>
-                    <Player :key="combKey" :ref="player" :urls="tracklist" :conf="playerconf"></Player>
+                <v-container>
+                    <v-sheet
+                    margin="auto"
+                    elevation="10"
+                    >
+                        <Player :key="combKey" :ref="player" :urls="tracklist" :conf="playerconf"></Player>
+                    </v-sheet>
+                    <p style="margin-top: 10px; color: black">Created with share.unmix.app</p>
+                </v-container>
                 </v-col>
             </v-row>
         </v-container>
@@ -29,7 +28,7 @@ export default {
     components: { Player },
     data () {
         return {
-            dark: true,
+            dark: false,
             player: null,
             combKey: 42,
             playerconf: {
@@ -74,3 +73,11 @@ export default {
     }
 }
 </script>
+
+
+<style lang="stylus">
+
+#player {
+  z-index: -1000
+}
+</style>
